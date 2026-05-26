d9cc3b09de565cfeb855ed743c4f292cbc1dd047
Oblyv <gregrmcbride@gmail.com>
2026-05-26 01:48:05 +0000

    test 1

--- src/host/controller.mjs
+++ src/host/controller.mjs
@@ -66,7 +66,6 @@ export class ProxyFrame {
     this.url = null;
     this.id = Math.random() + "";
     this.iframe = document.createElement("iframe");
-    this.iframe.sandbox = "allow-scripts allow-forms allow-modals allow-pointer-lock";
     this.iframe.allowFullscreen = true;
     this.iframe.setAttribute("frame-id", this.id);
 
